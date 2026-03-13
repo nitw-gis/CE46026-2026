e7a1149d5c8d60bc14397e3233a633f3f2ccc099
Charvi Sree <ncharvisree@gmail.com>
2026-03-13 13:40:50 +0530

    Update NISAR_Backscatter.py

--- 25CEM5R06/NISAR_Backscatter.py
+++ 25CEM5R06/NISAR_Backscatter.py
@@ -1,7 +1,7 @@
 """
 Author: Charvi Sree
 Roll Number: 25CEM5R06
-Course: CE46026 – THERMAL MICROWAVE AND HYPERSPECTRAL Remote Sensing
+Course: CE46026 – Thermal Microwave and Hyperspectral Remote Sensing
 
 Description:
 This script reads NISAR GCOV SAR data stored in HDF5 format and
@@ -9,8 +9,8 @@ visualizes the radar backscatter image. The VV polarization
 covariance term (VVVV) is extracted and converted to magnitude
 for visualization.
 
-The script also downsamples the raster to reduce memory usage
-and plots the SAR backscatter image using matplotlib.
+The raster is downsampled to reduce memory usage and displayed
+using matplotlib.
 """
 
 import h5py
@@ -22,7 +22,6 @@ import matplotlib.pyplot as plt
 # Path to NISAR HDF5 file
 # ------------------------------------
 file_path = r"D:\NTW\sem_2\THMRS\NISAR\NISAR_L2_PR_GCOV_010_164_D_077_2005_QPDH_A_20260120T140632_20260120T140648_X05010_N_P_J_001.h5"
-
 
 
 # ------------------------------------
@@ -81,31 +80,3 @@ plt.xlabel("Pixels")
 plt.ylabel("Pixels")
 
 plt.show()
-
-
-# reduce size for memory
-data = data[::10, ::10]
-
-# read coordinates
-x = f['science/LSAR/GCOV/grids/frequencyA/xCoordinates'][:]
-y = f['science/LSAR/GCOV/grids/frequencyA/yCoordinates'][:]
-
-xmin, xmax = x.min(), x.max()
-ymin, ymax = y.min(), y.max()
-
-transform = from_bounds(xmin, ymin, xmax, ymax, data.shape[1], data.shape[0])
-
-# export GeoTIFF (without CRS to avoid error)
-with rasterio.open(
-    "nisar3_backscatter.tif",
-    "w",
-    driver="GTiff",
-    height=data.shape[0],
-    width=data.shape[1],
-    count=1,
-    dtype=data.dtype,
-    transform=transform,
-) as dst:
-    dst.write(data,1)
-
-print("GeoTIFF created")
