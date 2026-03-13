ee6e5765e9c9483a45498df91b0150d1050390af
Charvi Sree <ncharvisree@gmail.com>
2026-03-13 09:32:28 +0530

    Create chandrayan.py

---
+++ 25CEM5R06/chandrayan.py
@@ -0,0 +1,13 @@
+"""
+Charvi Sree
+Roll Number: YOUR_ROLL_NUMBER
+Course: CE46026 - Thematic Remote Sensing
+Assignment: Chandrayaan-II Hyperspectral Data Plotting
+
+Description:
+This script reads Chandrayaan-II IIRS calibrated hyperspectral data
+(.qub data cube) using the associated .hdr file. The hyperspectral cube
+is converted into a 3D NumPy array and spectral responses for selected
+pixels are plotted using matplotlib.
+"""
+
